9c295da106f2bbc9226f53fcb9bc1fb2322c29a5
David Correa <dcorrea070@soy.sena.edu.co>
2024-09-13 06:09:33 -0500

    solucionando conflicto

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const controllerClientes = require('../controller/clientes.controller');
+const controllerHoteles0 = require('../controller/clientes.controller');
 //const controllerUsuarios = require('../controller/usuarios.controller');
 const path = require('path');
 //const stripe = require("stripe")(process.env.STRIPEKEY);
